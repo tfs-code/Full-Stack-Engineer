68cc7b621bdd727cec56e6ffdae021629b9cc8fe
tfs-code <tylerfishersmith@gmail.com>
2026-05-04 09:08:50 +0100

    dependencies

--- React/Adopt a Pet/src/App.js
+++ React/Adopt a Pet/src/App.js
@@ -1,19 +1,32 @@
-import HomePage from './pages/home';
-import SearchPage from './pages/search';
-import PetDetailsPage from './pages/detail';
-import PetDetailsNotFound from './pages/petDetailsNotFound';
-import Root from './components/root';
+import HomePage from "./pages/home";
+import SearchPage from "./pages/search";
+import PetDetailsPage from "./pages/detail";
+import PetDetailsNotFound from "./pages/petDetailsNotFound";
+import Root from "./components/root";
 
 // Add react-router-dom imports
+import {
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+} from "react-router-dom";
 
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Root />}>
+            <Route index element={<HomePage />} />
+            <Route path="/:type" element={<HomePage />} />
+        </Route>,
+    ),
+);
 
 function App() {
-  return (
-    // replace below with a Router Provider
-    <p>REPLACE ME</p>
-  );
+    return (
+        // replace below with a Router Provider
+        <RouterProvider router={appRouter} />
+    );
 }
 
 export default App;
